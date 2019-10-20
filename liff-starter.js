ddf10da61ba6247a851cc666920d5e4be5d471be
arwutr <arwutr@hotmail.com>
2019-10-20 11:52:22 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -31,8 +31,8 @@ function initializeApp(data) {
             text: "You've successfully sent a message! Hooray!"
         }, {
             type: 'sticker',
-            packageId: '2',
-            stickerId: '144'
+            packageId: '1274372',
+            stickerId: '11122055'
         }]).then(function () {
             window.alert("Message sent");
         }).catch(function (error) {
